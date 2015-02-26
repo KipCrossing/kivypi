86a156fcb904c5d8274fbc2ce670b65a173d4ed7
KipCrossing <kip.crossing@gmail.com>
2015-02-26 17:26:47 +1100

    Create install-kivypi.sh

---
+++ install-kivypi.sh
@@ -0,0 +1,13 @@
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo reboot
+
+sudo apt-get install libwayland0=0.85.0-2
+
+sudo apt-get install python-setuptools python-pygame python-opengl python-gst0.10 python-enchant gstreamer0.10-plugins-good python-dev build-essential libgl1-mesa-dev libgles2-mesa-dev python-pip
+sudo pip install cython==0.21
+sudo git clone git://github.com/kivy/kivy.git
+cd kivy
+sudo python setup.py build_ext --inplace -f
+sudo python setup.py install
