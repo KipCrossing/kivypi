a11c74fdf527177025acd2651c2659d462b9632b
kip <kip.crossing@gmail.com>
2016-10-06 11:33:29 +1100

    Cython version updated

--- install-kivypi.sh
+++ install-kivypi.sh
@@ -1,5 +1,5 @@
 sudo apt-get install python-setuptools python-pygame python-opengl python-dev build-essential python-pip -y
-sudo pip install cython==0.21
+sudo pip install cython==0.23
 sudo git clone git://github.com/kivy/kivy.git
 cd kivy
 sudo python setup.py build_ext --inplace -f
